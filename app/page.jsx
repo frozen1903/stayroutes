208cfc24823a06c83fdb39a29b19d82b90776fe1
frozen1903 <25480851@stu.omu.edu.tr>
2026-05-20 20:09:29 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,12 @@ export default function Home(){
       <Navbar/>
       <Hero/>
       <Services/>
+      <a
+  href="https://wa.me/905555555555"
+  className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-400 transition-all duration-300 text-white w-16 h-16 rounded-full flex items-center justify-center text-3xl shadow-2xl z-50"
+>
+  💬
+</a>
     </main>
   )
 }
