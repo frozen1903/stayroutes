41437335b4f61ac1e569fe6ad7ac6e69aa1fa8d5
frozen1903 <25480851@stu.omu.edu.tr>
2026-05-21 02:26:50 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero'
 import Services from '../components/Services'
 import Fleet from '../components/Fleet'
 import Footer from '../components/Footer'
+import Reviews from '../components/Reviews'
+
 
 export default function Home(){
   return(
@@ -12,6 +14,7 @@ export default function Home(){
       <Services/>
       <Fleet/>
       <Footer/>
+      <Reviews/>
 <a
   href="https://wa.me/90555555555"
   className="hidden md:flex fixed bottom-6 right-6 bg-green-500 hover:bg-green-400 transition-all duration-300 text-white w-16 h-16 rounded-full items-center justify-center text-3xl shadow-2xl z-50"
