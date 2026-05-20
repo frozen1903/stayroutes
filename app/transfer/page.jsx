a12bb0e81372f80353bae6860fc973006cd0abfe
frozen1903 <25480851@stu.omu.edu.tr>
2026-05-21 01:10:31 +0300

    Update page.jsx

--- app/transfer/page.jsx
+++ app/transfer/page.jsx
@@ -1,6 +1,7 @@
-"use client"
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+"use client"
+
 import { useState } from "react"
 
 export default function Transfer() {
